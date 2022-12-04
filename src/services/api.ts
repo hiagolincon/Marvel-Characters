d6fa0dcbc721ae85b59e58d2c341f3b7de13b72a
Hiago Dias Machado <hdiasmac@emeal.nttdata.com>
2022-12-04 00:46:12 -0300

    finish

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 import md5 from 'js-md5';
 
 const apiKey = (): { ts: number; apikey: string; hash: string } => {
-  const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY;
-  const PRIVATE_KEY = import.meta.env.VITE_PRIVATE_KEY;
+  const PUBLIC_KEY = 'be40780c1854261ee900fa2c17b576a9';
+  const PRIVATE_KEY = 'cc97a9b583ff905769ae44e03bb61a23022a5d13';
   const timestamp = Number(new Date());
   const hash = md5.create();
   hash.update(timestamp + PRIVATE_KEY + PUBLIC_KEY);
