53bb9e4a153de5ef780457ad579ca880625c2a8a
Hiago Dias Machado <hdiasmac@emeal.nttdata.com>
2022-12-04 00:33:27 -0300

    finish

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,20 +1,20 @@
-import axios from 'axios'
-import md5 from 'js-md5'
+import axios from 'axios';
+import md5 from 'js-md5';
 
 const apiKey = (): { ts: number; apikey: string; hash: string } => {
-  const PUBLIC_KEY = 'be40780c1854261ee900fa2c17b576a9'
-  const PRIVATE_KEY = 'cc97a9b583ff905769ae44e03bb61a23022a5d13'
-  const timestamp = Number(new Date())
-  const hash = md5.create()
-  hash.update(timestamp + PRIVATE_KEY + PUBLIC_KEY)
-  return { apikey: PUBLIC_KEY, ts: timestamp, hash: hash.hex() }
-}
+  const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY;
+  const PRIVATE_KEY = import.meta.env.VITE_PRIVATE_KEY;
+  const timestamp = Number(new Date());
+  const hash = md5.create();
+  hash.update(timestamp + PRIVATE_KEY + PUBLIC_KEY);
+  return { apikey: PUBLIC_KEY, ts: timestamp, hash: hash.hex() };
+};
 
-const params = apiKey()
+const params = apiKey();
 
 const api = axios.create({
   baseURL: 'https://gateway.marvel.com/v1/public/',
   params,
-})
+});
 
-export default api
+export default api;
