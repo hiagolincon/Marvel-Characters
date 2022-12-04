c6c9eee3ac2c85abced985655005e35ad1774e09
Hiago Dias Machado <hdiasmac@emeal.nttdata.com>
2022-12-04 00:21:31 -0300

    finish

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -1,30 +1,28 @@
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useState } from 'react';
 
 interface PaginationImp {
-  count: number
-  totalItems: number
-  totalPages: number
+  count: number;
+  totalItems: number;
+  totalPages: number;
 }
 interface ListContextData {
-  searchValue: string
-  pagination: PaginationImp
-  offset: number
-  isLoading: boolean
-  setSearchValue: (value: string) => void
-  setPagination: (value: PaginationImp) => void
-  setOffset: (value: number) => void
-  setIsLoading: (value: boolean) => void
+  searchValue: string;
+  pagination: PaginationImp;
+  offset: number;
+  isLoading: boolean;
+  setSearchValue: (value: string) => void;
+  setPagination: (value: PaginationImp) => void;
+  setOffset: (value: number) => void;
+  setIsLoading: (value: boolean) => void;
 }
 
-const ListContext = createContext<ListContextData>({} as ListContextData)
+const ListContext = createContext<ListContextData>({} as ListContextData);
 
-const ListProvider: React.FC = ({ children }) => {
-  const [searchValue, setSearchValue] = useState<string>('')
-  const [pagination, setPagination] = useState<PaginationImp>(
-    {} as PaginationImp,
-  )
-  const [offset, setOffset] = useState<number>(0)
-  const [isLoading, setIsLoading] = useState(true)
+const ListProvider = ({ children }: any) => {
+  const [searchValue, setSearchValue] = useState<string>('');
+  const [pagination, setPagination] = useState<PaginationImp>({} as PaginationImp);
+  const [offset, setOffset] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState(true);
 
   return (
     <ListContext.Provider
@@ -33,23 +31,23 @@ const ListProvider: React.FC = ({ children }) => {
         pagination,
         offset,
         isLoading,
-        setSearchValue: value => setSearchValue(value),
-        setPagination: value => setPagination(value),
-        setOffset: value => setOffset(value),
-        setIsLoading: value => setIsLoading(value),
+        setSearchValue: (value) => setSearchValue(value),
+        setPagination: (value) => setPagination(value),
+        setOffset: (value) => setOffset(value),
+        setIsLoading: (value) => setIsLoading(value),
       }}
     >
       {children}
     </ListContext.Provider>
-  )
-}
+  );
+};
 
 export const useList = (): ListContextData => {
-  const context = useContext(ListContext)
+  const context = useContext(ListContext);
   if (!context) {
-    throw new Error('useList must be user within an ListProvider')
+    throw new Error('useList must be user within an ListProvider');
   }
-  return context
-}
+  return context;
+};
 
-export default ListProvider
+export default ListProvider;
